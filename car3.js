a8ce6b4c50c3db8e6ce32eb8f137fa2e530ed0a0
unknown <Matheus.santostsis@gmail.com>
2023-12-19 10:01:31 -0300

    Criada a tercera parte do exercicio car

---
+++ car3.js
@@ -0,0 +1,10 @@
+//criar os objetos 
+
+const carro = new Carro('Peugeot', 'Sonic', 2013, 4);
+const moto = new Moto('Yamaha', '5600', 2021, '2 tempos');
+ 
+//chamar o metodo para um log da tela 
+
+    console.log(carro.descrever());
+    console.log(moto.descrever());
+  
